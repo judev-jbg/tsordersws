3d54068f4ebdf7e3537751e2cb5e44971ba771ef
Junior Balcazar - Judev <junior.marketing@selk.es>
2024-05-24 14:58:52 +0200

    update: Se incluyen los archivos requeridos para la logica

--- index.php
+++ index.php
@@ -1,23 +1,23 @@
 <?php
 
 error_reporting(E_ALL);
-ini_set('ignore_repeated_errors',TRUE);
-ini_set('display_errors',FALSE);
-ini_set('log_errors',TRUE);
-ini_set('error_log',"C:\\xampp\\htdocs\\tsordersws\\traking.log");
+ini_set('ignore_repeated_errors', TRUE);
+ini_set('display_errors', FALSE);
+ini_set('log_errors', TRUE);
+ini_set('error_log', "C:\\xampp\\htdocs\\tsordersws\\traking.log");
 error_log("====================================================");
 
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-
-//require_once "conf/config.php";
-//require_once "libs/database.php";
-//require_once "libs/app.php";
+require_once "config/apikey.php";
+require_once "config/routes.php";
+require_once "config/database.php";
+require_once "libs/Database.php";
+require_once "models/OrderModel.php";
+require_once "controllers/OrderController.php";
+require_once "routes/Route.php";
 
 $route = new Route();
 $route->handleRequest();
-
-?>
