9bca598779d192af085f7cc5bf034ba587be260a
Junior Balcazar - Judev <junior.marketing@selk.es>
2024-05-24 15:46:39 +0200

    feat: Se define la estructura de metodos

--- controllers/OrderController.php
+++ controllers/OrderController.php
@@ -1,14 +1,44 @@
 <?php
 
-class OrderController{
+class OrderController
+{
 
+    private $orderModel;
 
-
-    public function __construct() {
-    
+    public function __construct()
+    {
+        $this->orderModel = new Order();
     }
-    
-    
-    }
 
-?>
+    public function getOrderById($id)
+    {
+    }
+    public function getOrdersPending()
+    {
+    }
+    public function getOrdersPendingUntilToday()
+    {
+    }
+    public function getOrderOutOfStock()
+    {
+    }
+    public function getOrderOutOfStockUntilToday()
+    {
+    }
+    public function getOrdersHistory()
+    {
+    }
+    public function generateOrdersFileFromFileName($data)
+    {
+    }
+    public function registerShipment($data)
+    {
+        //Validar el tipo de envio (ShipmentType = > [usingFile, usingWS])
+    }
+    private function generateOrdersFile($data)
+    {
+    }
+    private function sendShipmentWS($data)
+    {
+    }
+}
